468b0eb4283c0c5fea2d8cf05a5c42e0a358f174
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2024-01-05 18:48:38 +0000

    added stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template ='checkout/checkout.html'
     context = {
         'order_form': order_form,
-    }
+        'stripe_public_key': 'pk_test_51OVHBjGFQTrirOtmFnX4aMBu8vYGfu72xgLAPbHrkZjlqnDJ8V87M7Rj3GCBTgCAHxnbtHRC10mN4txOHZLjx2pX00P0FzIcml',
+        'client_secret': 'test client secret',
+        }
 
     return render(request, template, context)
