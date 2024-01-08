6c64441c3609ae85f8d4a157d11402833e590399
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2024-01-08 22:36:52 +0000

    settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-b1ndark-boutiqueadov1-2w65n0nacom.ws-eu107.gitpod.io', 'boutiqueado10-2311e87e8a55.herokuapp.com']
 
